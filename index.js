0f286ad9143b4728ab457c0b63ef1e43bde360a6
Addie Brunk <mlbrunk23@gmail.com>
2018-05-28 17:18:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name){
 
   kittens = ['Milo', 'Otis', 'Garfield'];
-
   kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name){
+  kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens.unshift(name)
   return kittens
 }
